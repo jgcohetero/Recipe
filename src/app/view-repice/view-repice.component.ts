101c854991636fab65fa11362a506c492ccf065e
jgcohetero <cohetero745@gmail.com>
2020-02-28 10:29:54 -0600

    finish

--- src/app/view-repice/view-repice.component.ts
+++ src/app/view-repice/view-repice.component.ts
@@ -9,7 +9,11 @@ import { MealDBService } from '../services/meal-db.service';
 })
 export class ViewRepiceComponent implements OnInit {
 id:any;
+<<<<<<< Updated upstream
 meals:any;
+=======
+mealsId:any;
+>>>>>>> Stashed changes
   constructor(private route:ActivatedRoute,private meal:MealDBService) {
     this.id = this.route.snapshot.paramMap.get('id');
     console.log(this.id)
@@ -19,13 +23,35 @@ meals:any;
    async search(id:any){
     await this.meal.getIdMeal(id).
     subscribe((res)=>{
+<<<<<<< Updated upstream
       this.meals=res.meals;
       console.log(this.meals)
+=======
+      var jsonArray = JSON.parse(JSON.stringify(res))
+      console.log(jsonArray)
+>>>>>>> Stashed changes
     })
    }
   ngOnInit() {
+<<<<<<< Updated upstream
   
   }
 
+=======
+    this.getRecetaId()
+  }
+
+  async getRecetaId(){
+    this.id = this.route.snapshot.paramMap.get('id');
+    // console.log(this.id)
+   await this.meal.getIdMeal(this.id).
+    subscribe((res)=>{
+      // console.log(res)
+      this.mealsId = res.meals;
+      // console.log(this.mealsId)
+    })
+  }
+
+>>>>>>> Stashed changes
 
 }
